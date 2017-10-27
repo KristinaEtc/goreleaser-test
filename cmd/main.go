19a2a6b0265f0a84f537005f7670d17ec5d5b74b
Kristina Kovalevskaya <kristina.kovalevskaya@oxagile.com>
2017-10-27 14:04:18 +0300

    working build

--- cmd/main.go
+++ cmd/main.go
@@ -1,56 +1,22 @@
 package main
 
 import (
-	"io/ioutil"
 	"log"
 	"os"
 
 	"github.com/KristinaEtc/goreleaser"
-	"github.com/goreleaser/goreleaser/config"
-	yaml "gopkg.in/yaml.v2"
 )
-
-const (
-	confFile              = ".goreleaser.yml"
-	dirWithGitHubReleases = ".dist"
-	secret                = "secret"
-)
-
-func PushToGithub() error {
-	return nil
-}
-
-func ParseGitHubConf(confName string) (*config.Project, error) {
-	b, err := ioutil.ReadFile(confFile)
-	if err != nil {
-		return nil, err
-	}
-
-	var conf config.Project
-	err = yaml.Unmarshal(b, &conf)
-	if err != nil {
-		return nil, err
-	}
-	return &conf, err
-}
-
-func Release() error {
-	conf, err := ParseGitHubConf(confFile)
-	if err != nil {
-		return err
-	}
-	log.Printf("conf: %+v\n", conf)
-	if err := goreleaser.RunBuild(conf); err != nil {
-		log.Fatal(err.Error())
-	}
-
-	return nil
-}
 
 func main() {
-	if err := Release(); err != nil {
-		log.Println("ERR [parseGitHubConf config] ", err.Error())
+
+	conf, err := goreleaser.ParseGitHubConf("../example/.goreleaser.yml")
+	if err != nil {
+		log.Println("ERR [ParseGitHubConf]: ", err.Error())
+	}
+
+	if err := goreleaser.Release(conf); err != nil {
+		log.Println("ERR [Release]: ", err.Error())
 		os.Exit(1)
 	}
-	log.Println("Done successfully")
+	log.Println("INFO Done successfully")
 }
